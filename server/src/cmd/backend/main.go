874c9ef339f0a9ae947e93c4420df4c2b7ca330e
kotalab <kotarolab@gmail.com>
2019-05-10 22:28:11 +0900

    initial commit

---
+++ server/src/cmd/backend/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
